b1c4cc7c076409182ee596d9869eabc382f6b574
Mathieu Halle <m@bertha.ca>
2022-01-13 13:32:01 -0500

    Adding old version and dev info in comments

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
+// /*
+//     Inlude our library
+// */
+
+// const mqtt = require("mqtt");
+
+// const client = mqtt.connect("mqtt://mqtt.thingsboard.cloud", {
+//   // port: 8883,
+//   // rejectUnauthorized: true,
+//   debug: true,
+//   clean: true,
+//   username: "HomHUDEykBxg36I85S40", // Xp4VqqZbulh31aT4XJ4K
+//   // protocolId: "MQIsdp",
+//   // protocolVersion: 3,
+//   // connectTimeout: 10,
+// });
+
+// // console.log(this.options.mqtt.host, this.options.mqtt.config);
+
+// client.on("error", (err) => {
+//   console.log(" [STATUS]  MQTTError error", err);
+//   // this.onMQTTError(err);
+//   // this.client.end();
+// });
+
+// client.on("connect", (akn) => {
+//   console.log(akn);
+//   // this.onMQTTConnect();
+// });
+
+// client.on("reconnect", () => {
+//   console.log(" [STATUS]  MQTTError reconnect");
+// });
+
+// client.on("message", (topic, message) => {
+//   this.onMQTTMessage(topic, message);
+// });
+// }
+
 const Device = require("./src/devices");
+//
+
+// # for ThingsBoard Cloud
+
+// # Publish data as an object without timestamp (server-side timestamp will be used)
+// mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "HomHUDEykBxg36I85S40" -m "{"temperature":42}"
+// # Publish data as an object without timestamp (server-side timestamp will be used) using data from file
+// mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-object.json"
+// # Publish data as an array of objects without timestamp (server-side timestamp will be used)  using data from file
+// mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-array.json"
+// # Publish data as an object with timestamp (telemetry timestamp will be used)  using data from file
+// mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-with-ts.json"
+
+// # for local ThingsBoard
+
+// # Publish data as an object without timestamp (server-side timestamp will be used)
+// mosquitto_pub -d -q 1 -h "127.0.0.1" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -m "{"temperature":42}"
+// # Publish data as an object without timestamp (server-side timestamp will be used) using data from file
+// mosquitto_pub -d -q 1 -h "127.0.0.1" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-object.json"
+// # Publish data as an array of objects without timestamp (server-side timestamp will be used) using data from file
+// mosquitto_pub -d -q 1 -h "127.0.0.1" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-array.json"
+// # Publish data as an object with timestamp (telemetry timestamp will be used) using data from file
+// mosquitto_pub -d -q 1 -h "127.0.0.1" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-with-ts.json"
+
+//no1kPj0qH0iWGNyykQNu
 
 const numberOfPin = 40;
 const clientAttrs = [];
@@ -11,11 +75,13 @@ for (let i = 1; i <= numberOfPin; i++) {
   });
 }
 
+// console.log(clientAttrs);
+
 const rpi = new Device({
   mqtt: {
     host: "mqtt://mqtt.thingsboard.cloud", // thingsboard.cloud
     config: {
-      username: "", // Xp4VqqZbulh31aT4XJ4K
+      username: "DKA85BcZPI6O1ZJ8hb60", // Xp4VqqZbulh31aT4XJ4K
     },
   },
   attributes: {
@@ -54,3 +120,181 @@ const rpi = new Device({
     },
   ],
 });
+
+// const aaa = new Device({
+//   mqtt: {
+//     host: "mqtt://mqtt.thingsboard.cloud", // thingsboard.cloud
+//     config: {
+//       username: "DKA85BcZPI6O1ZJ8hb60", // Xp4VqqZbulh31aT4XJ4K
+//     },
+//   },
+//   rpc: [
+//     {
+//       name: "rpcCommand_2",
+//       action: function (requestId, attr, device) {
+//         console.log(
+//           "RPC rpcCommand_2 ACTION *************************************************",
+//           attr
+//           // device.telemetry
+//         );
+
+//         // Take action on telemetries
+//         let obj = device.telemetry.map((o, i, arr) => {
+//           if (o.key === "batAmp") {
+//             arr[i].value = 111;
+//           }
+//           if (o.key === "batLvl") {
+//             arr[i].value = 999;
+//           }
+//         });
+
+//         device.postDeviceTelemetries();
+
+//         let prevValue = parseFloat(device.attributes.client.Input1) || 0;
+//         device.attributes.client.Input1 = prevValue + 1;
+//         device.postClientDeviceAttributes();
+
+//         // Post RPC response
+//         device.postDeviceRPCResponse(requestId, attr);
+//       },
+//     },
+//     {
+//       name: "setGpioStatus",
+//       action: function (requestId, attr, device) {
+//         console.log(
+//           "2222 RPC ACTION *************************************************",
+//           attr
+//           // device
+//         );
+
+//         // Post RPC response
+//         device.postDeviceRPCResponse(requestId, {
+//           1: true,
+//           2: false,
+//           3: false,
+//           4: true,
+//         });
+//       },
+//     },
+//     {
+//       name: "getGpioStatus",
+//       action: function (requestId, attr, device) {
+//         console.log(
+//           "2222 RPC ACTION *************************************************",
+//           attr
+//           // device
+//         );
+
+//         // Post RPC response
+//         device.postDeviceRPCResponse(requestId, {
+//           1: false,
+//           2: true,
+//           3: true,
+//           4: true,
+//         });
+//       },
+//     },
+//   ],
+//   attributes: {
+//     client: [
+//       {
+//         name: "Input 1",
+//         key: "Input1",
+//         value: "Input 5 Value Client",
+//         onUpdate: function (attr, device) {
+//           console.log(" >>>> >> ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] ----- [[");
+//         },
+//       },
+//       {
+//         name: "Input 2",
+//         key: "Input2",
+//         value: "Input 2 Value",
+//       },
+//     ],
+//     shared: [
+//       {
+//         name: "Input 1",
+//         key: "Input1",
+//         value: "Input 5 Value Shared",
+//         onUpdate: function (attr, device) {
+//           console.log(" >>>> << ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] ----- [[");
+//         },
+//       },
+//       {
+//         name: "Input S1",
+//         key: "InputS1",
+//         value: "Input S1 Value",
+//         onUpdate: function (attr, device) {
+//           console.log("]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] ----- [[");
+//         },
+//       },
+//       {
+//         name: "Input S2",
+//         key: "InputS2",
+//         value: "Input S2 Value",
+//         onUpdate: function (attr, device) {
+//           console.log("555555]]]]]]]]]]]]]]]]]]]]]]]]] ----- [[");
+//         },
+//       },
+//     ],
+//   },
+//   telemetry: [
+//     {
+//       key: "batLvl",
+//       sendInterval: 10000,
+//       value: 3000,
+//       emulator: function (prevValue) {
+//         return Math.floor(Math.random() * 100);
+//       },
+//     },
+//     {
+//       key: "batAmp",
+//       sendInterval: 10000,
+//       value: 150,
+//       emulator: function (prevValue) {
+//         return Math.random() + 1 + prevValue;
+//       },
+//     },
+//   ],
+// });
+
+// console.log(aaa);
+
+// // Demo devices
+// let devices_to_fake = [
+//   "veRsOqA8SdK5t23ShQhB",
+//   "MQjzRg27rpUFF5zzNvy4",
+//   "EA2hpZUuXAlmbK2gza2c",
+//   "1Heb7lZe6FTz403Fw4cl",
+// ];
+
+// const mqtt_host = "mqtt://tb.bertha.co";
+
+// // Demo devices
+// let devices_to_fake = [
+//   "fwkQOwCRGsaXavMR5l4T",
+//   "xu9IWSD4ocPzgtKPOyOY",
+//   "ZfkeS2heDRouuiyk5pJU",
+// ];
+
+// const devices_arr = [];
+
+// for (let i = 0, l = devices_to_fake.length; i < l; i++) {
+//   let temp = new Kaliflah({
+//     username: devices_to_fake[i],
+//     host: mqtt_host,
+//     logLevel: 0,
+//   });
+
+//   // temp.fakeUsage(temp);
+
+//   devices_arr.push(temp);
+// }
+
+// let temp = new Kaliflah({
+//   username: "fwkQOwCRGsaXavMR5l4T",
+//   host: mqtt_host,
+//   logLevel: 0,
+// });
+
+// console.log(devices_arr);
--- src/devices/index.js
+++ src/devices/index.js
@@ -308,3 +308,644 @@ class Device {
 }
 
 module.exports = Device;
+
+// const mqtt = require("mqtt");
+// // const mqtt = require("mqtt");
+
+// class Kaliflah {
+//   constructor(options) {
+//     if (options.hasOwnProperty("host")) {
+//       this.host = options.host;
+//     }
+
+//     if (options.hasOwnProperty("username")) {
+//       this.username = options.username;
+//     }
+
+//     if (options.hasOwnProperty("versionSW")) {
+//       this.versionSW = options.versionSW;
+//     } else {
+//       this.versionSW = "1.0";
+//     }
+
+//     if (options.hasOwnProperty("versionHW")) {
+//       this.versionHW = options.versionHW;
+//     } else {
+//       this.versionHW = "1.0";
+//     }
+
+//     if (options.hasOwnProperty("versionBL")) {
+//       this.versionBL = options.versionBL;
+//     } else {
+//       this.versionBL = "2.0";
+//     }
+
+//     if (options.hasOwnProperty("logLevel")) {
+//       this.logLevel = options.logLevel;
+//     } else {
+//       this.logLevel = 1;
+//     }
+
+//     this.comRoutineTimeout = false;
+//     this.comRoutineTimeoutDelay = 15000;
+
+//     this.isMqttConnected = false;
+
+//     this.now = new Date();
+
+//     this.attr = {
+//       upTime: this.now.toISOString(),
+//       isIn1Connected: true,
+//       isIn2Connected: true,
+//       isIn3Connected: true,
+//       isIn4Connected: true,
+//       versionSW: this.versionSW,
+//       versionHW: this.versionHW,
+//       versionBL: this.versionBL,
+//       versionSWLatest: null,
+//     };
+
+//     this.usageLog = [];
+
+//     this.slaves = [];
+
+//     this.outputStatus = {
+//       out1: false,
+//       out2: false,
+//       out3: false,
+//       out4: false,
+//     };
+
+//     this.attributesRequestId = 1;
+
+//     this.generateRandomTelemetry();
+
+//     this.init();
+//   }
+
+//   init() {
+//     if (this.host) {
+//       this.log(
+//         "*************************** Init started ***************************",
+//         100
+//       );
+
+//       this.log("User: " + this.username, 100);
+
+//       this.client = mqtt.connect(this.host, {
+//         username: this.username,
+//         // rejectUnauthorized: false,
+//         // port: 8883,
+//         // // debug: true,
+//         // // protocolId: "MQIsdp",
+//         // // protocolVersion: 3,
+//         // connectTimeout: 10,
+//       });
+
+//       this.client.stream.on("error", (err) => {
+//         this.log("!!!!! error !!!!!", 100);
+//         this.log(err, 100);
+//         this.client.end();
+//         console.log("######6666####", err);
+//       });
+
+//       this.client.on("connect", () => {
+//         console.log("#########55#");
+//         this.log(
+//           "*************************** Master connected ***************************",
+//           1
+//         );
+//         this.log(this.host, 1);
+
+//         this.log(
+//           "************************************************************************",
+//           1
+//         );
+//         this.onConnect();
+//         this.isMqttConnected = true;
+//       });
+
+//       this.client.on("message", (topic, message) => {
+//         this.log("", 1);
+//         this.log("", 1);
+//         this.log("[*] onMessage --------------------", 1);
+//         this.log("[*] Topic: " + topic, 1);
+//         this.log("[*] message: " + message.toString(), 1);
+//         this.log("**********", 1);
+
+//         if (topic === "v1/gateway/rpc") {
+//           this.onSlaveMessage(topic, message);
+//         } else {
+//           this.onMessage(topic, message);
+//         }
+//       });
+//     }
+//   }
+
+//   onConnect() {
+//     // subscribe to the various topics we'll use
+
+//     // get updatess on attributes update
+//     this.client.subscribe("v1/devices/me/attributes");
+//     this.client.subscribe("v1/devices/me/attributes/response/+");
+
+//     // getting the commands requests
+//     this.client.subscribe("v1/devices/me/rpc/request/+");
+
+//     // get slave attr
+//     this.client.subscribe("v1/gateway/attributes");
+
+//     // get slave commands
+//     this.client.subscribe("v1/gateway/rpc/+");
+
+//     console.log(
+//       "*******************************************************************************"
+//     );
+//     console.log(
+//       "*******************************************************************************"
+//     );
+//     console.log(
+//       "*******************************************************************************"
+//     );
+//     console.log(
+//       "*******************************************************************************"
+//     );
+//     console.log(
+//       "*******************************************************************************"
+//     );
+//     console.log(
+//       "*******************************************************************************"
+//     );
+//     console.log(
+//       "*******************************************************************************"
+//     );
+
+//     // Send data on init
+//     this.postTelemetry();
+//     this.postAttributes();
+//     this.requestMasterAttr(
+//       "versionSWLatest,versionSWLatestURL,versionSWLatestChecksum,versionSWUpdateAfter,active"
+//     );
+//     this.startComRoutine();
+//     console.log("onConnect");
+//   }
+
+//   requestMasterAttr(key) {
+//     this.attributesRequestId++;
+//     this.client.publish(
+//       "v1/devices/me/attributes/request/" + this.attributesRequestId,
+//       '{"sharedKeys":"' + key + '"}'
+//     );
+//   }
+
+//   checkForUpdates() {
+//     // console.log(
+//     //   "Checking for updates ...",
+//     //   this.attr.versionSW,
+//     //   this.attr.versionSWLatest
+//     // );
+//     // console.log(this.attr);
+//     if (this.attr.versionSW !== this.attr.versionSWLatest) {
+//       // console.log("Updated needed");
+//       this.attr.versionSW = this.attr.versionSWLatest;
+//       this.postAttributes();
+//     } else {
+//       // console.log("Up to date");
+//     }
+//   }
+
+//   onMessage(topic, message) {
+//     let msgData = JSON.parse(message.toString());
+
+//     if (topic === "v1/devices/me/attributes") {
+//       this.attr = { ...this.attr, ...msgData };
+//       this.checkForUpdates();
+
+//       console.log(
+//         "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",
+//         topic,
+//         msgData,
+//         "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n"
+//       );
+
+//       return true;
+//     }
+
+//     if (topic.indexOf("v1/devices/me/attributes/response/") !== -1) {
+//       var requestId = topic.slice("v1/devices/me/attributes/response/".length);
+//       // console.log("msgData", msgData);
+//       this.attr = { ...this.attr, ...msgData.shared };
+//       this.checkForUpdates();
+
+//       console.log(
+//         "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",
+//         topic,
+//         msgData,
+//         "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n"
+//       );
+
+//       return true;
+//     }
+
+//     var requestId = topic.slice("v1/devices/me/rpc/request/".length);
+
+//     switch (msgData.method) {
+//       case "postTelemetry":
+//         msgData.params = { status: true };
+//         this.postTelemetry();
+//         break;
+
+//       case "postAttributes":
+//         msgData.params = { status: true };
+//         this.postAttributes();
+//         break;
+
+//       case "postUsageLog":
+//         msgData.params = { status: true };
+//         this.postUsageLog();
+//         break;
+
+//       case "setOutputStatus":
+//         this.setOutputStatus(msgData.params);
+//         msgData.params = { status: true };
+//         break;
+
+//       case "setFlashPatternStatus":
+//         this.setFlashPatternStatus(msgData.params);
+//         msgData.params = { status: true };
+//         break;
+
+//       case "getOutputStatus":
+//         msgData.params = this.outputStatus;
+//         break;
+
+//       default:
+//         msgData.params = { status: true };
+//         break;
+//     }
+
+//     this.log("", 1);
+//     this.log("**********", 1);
+//     this.log("[M] onMessage response --------------------", 1);
+//     this.log("[M] message: " + JSON.stringify(msgData.params), 1);
+//     this.log(
+//       "----------------------------------------------------------------",
+//       1
+//     );
+//     this.log("", 1);
+
+//     this.client.publish(
+//       "v1/devices/me/rpc/response/" + requestId,
+//       JSON.stringify(msgData.params)
+//     );
+//   }
+
+//   onSlaveMessage(topic, message) {
+//     let msgData = JSON.parse(message.toString());
+//     let device = this.slaves[msgData.device];
+//     let data = { status: true };
+
+//     switch (msgData.data.method) {
+//       case "postTelemetry":
+//         this.postSlaveTelemetry(device);
+//         break;
+
+//       case "postAttributes":
+//         this.postSlaveAttributes(device);
+//         break;
+
+//       case "postUsageLog":
+//         this.postSlaveUsageLog(device);
+//         break;
+
+//       case "setOutputStatus":
+//         this.log(device);
+//         device.setOutputStatus(msgData.data.params);
+//         break;
+
+//       case "getOutputStatus":
+//         this.log("[S] Posting output status");
+//         data = device.outputStatus;
+//         break;
+
+//       default:
+//         msgData.params = { status: true };
+//         break;
+//     }
+
+//     let msg = {
+//       device: msgData.device,
+//       id: msgData.data.id,
+//       data: data,
+//     };
+//     this.log("", 1);
+//     this.log("**********", 1);
+//     this.log("[S] onSlaveMessage response --------------------", 1);
+//     this.log("[S] message: " + JSON.stringify(msg), 1);
+//     this.log(
+//       "----------------------------------------------------------------",
+//       1
+//     );
+//     this.log("", 1);
+//     this.client.publish("v1/gateway/rpc", JSON.stringify(msg));
+//   }
+
+//   startComRoutine() {
+//     let that = this;
+//     console.log("COM ROUTINE !!!!!!");
+//     this.comRoutine(this);
+
+//     this.comRoutineTimeout = setTimeout(() => {
+//       this.startComRoutine();
+//     }, this.comRoutineTimeoutDelay);
+//   }
+
+//   comRoutine(device) {
+//     if (device.host) {
+//       device.postTelemetry();
+//       device.postAttributes();
+//     } else {
+//       device.postSlaveAttributes(device);
+//       device.postSlaveTelemetry(device);
+//     }
+//     device.fakeUsage(device);
+//   }
+
+//   /* ----
+//       Slave devices notifications
+// */
+
+//   fakeUsage(device) {
+//     if (device.host) {
+//       // this.postTelemetry();
+//       this.postAttributes();
+//       this.generateFakeUsageLog(this);
+//       this.postUsageLog();
+//     } else {
+//       // this.postSlaveAttributes(device);
+//       // this.postSlaveTelemetry(device);
+//       this.generateFakeUsageLog(device);
+//       this.postSlaveUsageLog(device);
+//     }
+//   }
+
+//   generateFakeUsageLog(device) {
+//     const optionList = ["out1", "out2", "out3", "out4"];
+
+//     let usageCount = Math.floor(Math.random() * 5);
+
+//     for (let i = 0; i < usageCount; i++) {
+//       setTimeout(() => {
+//         device.addToUsageLog(
+//           optionList[Math.floor(Math.random() * optionList.length)]
+//         );
+//       }, usageCount * i);
+//     }
+//   }
+
+//   connectSlaveDevice(device) {
+//     if (this.isMqttConnected) {
+//       this.slaves = { ...this.slaves, [device.username]: device };
+//       let msg = { device: device.username };
+//       this.log("", 1);
+//       this.log("[M] Connecting slave device: " + JSON.stringify(msg), 1);
+//       this.client.publish("v1/gateway/connect", JSON.stringify(msg));
+//       this.keepAliveSlaveDevice(device);
+
+//       this.postSlaveAttributes(device);
+//       this.postSlaveTelemetry(device);
+//     }
+//   }
+
+//   keepAliveSlaveDevice(device) {
+//     let intervall = setInterval(() => {
+//       let msg = { device: device.username };
+//       this.client.publish("v1/gateway/connect", JSON.stringify(msg));
+//     }, 20000);
+//   }
+
+//   setOutputStatus(args) {
+//     Object.keys(args).forEach((key) => {
+//       this.log(key + ": " + args[key], 1);
+//       this.log(
+//         "[*] Changing the status for output: " + key,
+//         args[key].status,
+//         1
+//       );
+//       this.outputStatus[key] = args[key].status ? args[key].status : false;
+//       this.addToUsageLog(key);
+//     });
+
+//     this.postAttributes();
+//   }
+
+//   setFlashPatternStatus(args) {
+//     Object.keys(args).forEach((key) => {
+//       this.log(key + ": " + args[key], 1);
+//       this.log("[*] Changing the status for output: " + key, args[key], 1);
+//       this.outputStatus[key] = args[key] ? args.type : false;
+//       this.addToUsageLog(key);
+//     });
+
+//     this.postAttributes();
+//   }
+
+//   addToUsageLog(output) {
+//     let logs = [];
+
+//     logs.push({
+//       ts: new Date().getTime() + Math.floor(Math.random() * 12),
+//       values: {
+//         input: ["remote"],
+//         output: [output],
+//       },
+//     });
+
+//     this.usageLog = [...this.usageLog, ...logs];
+//   }
+
+//   /* ----
+//       Functions to generate random attributes
+// */
+
+//   getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.random() * (max - min + 1) + min;
+//   }
+
+//   generateRandomTelemetry() {
+//     let d = new Date();
+//     let n = d.getHours();
+
+//     let randomTelemetry = {
+//       vBat: this.getRandomInt(9, 13).toFixed(2),
+//       iBat: this.getRandomInt(2, 4).toFixed(2),
+//       vChr: this.getRandomInt(12, 15).toFixed(2),
+//       iChr: this.getRandomInt(4, 5).toFixed(2),
+//       iOut: this.getRandomInt(0, 2).toFixed(2),
+//       temp: this.getRandomInt(n, n * 0.3).toFixed(2),
+//       light: this.getRandomInt(n, n * 1.2).toFixed(),
+//       rssi: this.getRandomInt(n, n).toFixed(),
+//     };
+
+//     this.telemetry = { ...this.telemetry, ...randomTelemetry };
+//   }
+
+//   generateRandomAttributes() {
+//     const locationList = [
+//       { lat: 50.225037, lng: -66.361017 },
+//       { lat: 50.206361, lng: -66.367471 },
+//       { lat: 50.212566, lng: -66.37723 },
+//       { lat: 50.202348, lng: -66.369547 },
+//       { lat: 50.200108, lng: -66.375493 },
+//       { lat: 50.204247, lng: -66.378576 },
+//       { lat: 50.218646, lng: -66.369004 },
+//       { lat: 50.226875, lng: -66.358913 },
+//       { lat: 50.221956, lng: -66.355596 },
+//       { lat: 50.223043, lng: -66.35039 },
+//       { lat: 50.220855, lng: -66.356554 },
+//       { lat: 50.23089, lng: -66.39293 },
+//       { lat: 50.235487, lng: -66.383534 },
+//       { lat: 50.229126, lng: -66.387986 },
+//       { lat: 50.224032, lng: -66.403169 },
+//       { lat: 50.217799, lng: -66.402547 },
+//       { lat: 50.215911, lng: -66.402644 },
+//       { lat: 50.213026, lng: -66.397955 },
+//       { lat: 50.212333, lng: -66.397325 },
+//       { lat: 50.209868, lng: -66.38974 },
+//       { lat: 50.206256, lng: -66.388485 },
+//       { lat: 50.202314, lng: -66.383936 },
+//       { lat: 50.216644, lng: -66.382039 },
+//     ];
+
+//     // let randomAttributes =
+//     //   locationList[Math.floor(Math.random() * locationList.length)];
+
+//     let randomAttributes = {};
+
+//     this.generateRandomInput();
+
+//     this.attr = { ...this.attr, ...randomAttributes };
+//   }
+
+//   generateRandomInput() {
+//     const optionList = [true, false];
+
+//     let randmonInputStatus = {
+//       isIn1Connected: optionList[Math.floor(Math.random() * optionList.length)],
+//       isIn2Connected: optionList[Math.floor(Math.random() * optionList.length)],
+//       isIn3Connected: optionList[Math.floor(Math.random() * optionList.length)],
+//       isIn4Connected: optionList[Math.floor(Math.random() * optionList.length)],
+//       isOut1Active: this.outputStatus.out1,
+//       isOut2Active: this.outputStatus.out2,
+//       isOut3Active: this.outputStatus.out3,
+//       isOut4Active: this.outputStatus.out4,
+//     };
+
+//     this.attr = { ...this.attr, ...randmonInputStatus };
+//   }
+
+//   /* ----
+//       Posting data to the server
+// */
+
+//   postAttributes() {
+//     this.generateRandomAttributes();
+//     this.log("", 1);
+//     this.log("[M] Posting Attributes: ", JSON.stringify(this.attr), 1);
+//     this.client.publish(
+//       "v1/devices/me/attributes",
+//       JSON.stringify(this.attr),
+//       1
+//     );
+//   }
+
+//   postTelemetry() {
+//     this.generateRandomTelemetry();
+//     this.log("", 1);
+//     console.log(this.telemetry);
+//     console.log("[M] Posting telemetry: ", JSON.stringify(this.telemetry), 1);
+//     this.client.publish(
+//       "v1/devices/me/telemetry",
+//       JSON.stringify(this.telemetry)
+//     );
+//   }
+
+//   postUsageLog() {
+//     this.log("", 1);
+//     this.log("[M] Posting Usage Log: " + JSON.stringify(this.usageLog), 1);
+
+//     this.client.publish(
+//       "v1/devices/me/telemetry",
+//       JSON.stringify(this.usageLog)
+//     );
+//     this.usageLog = [];
+//   }
+
+//   postSlaveTelemetry(device) {
+//     device.generateRandomTelemetry();
+//     let msg = {
+//       [device.username]: [
+//         {
+//           ts: new Date().getTime(),
+//           values: {
+//             ...device.telemetry,
+//           },
+//         },
+//       ],
+//     };
+
+//     this.log("", 1);
+//     this.log("[S] Posting Telemetry: ", JSON.stringify(msg), 1);
+
+//     this.client.publish("v1/gateway/telemetry", JSON.stringify(msg));
+//   }
+
+//   postSlaveAttributes(device) {
+//     device.generateRandomAttributes();
+
+//     let msg = {
+//       [device.username]: device.attr,
+//     };
+
+//     this.log("", 1);
+//     this.log("[S] Posting Attributes: ", JSON.stringify(msg), 1);
+
+//     // this.client.publish("v1/gateway/attributes", JSON.stringify(msg));
+//   }
+
+//   postSlaveUsageLog(device) {
+//     device.addToUsageLog("out1");
+
+//     let msg = {
+//       [device.username]: [...device.usageLog],
+//     };
+
+//     this.log("", 1);
+//     this.log("[S] Posting Usage Log: ", JSON.stringify(msg), 1);
+
+//     this.client.publish("v1/gateway/telemetry", JSON.stringify(msg));
+
+//     device.usageLog = [];
+//   }
+
+//   log(msg, lvl) {
+//     if (lvl >= this.logLevel) {
+//       console.log(msg);
+//     }
+//   }
+
+//   formatAMPM(date) {
+//     var hours = date.getHours();
+//     var minutes = date.getMinutes();
+//     var ampm = hours >= 12 ? "pm" : "am";
+//     hours = hours % 12;
+//     hours = hours ? hours : 12; // the hour '0' should be '12'
+//     minutes = minutes < 10 ? "0" + minutes : minutes;
+//     var strTime = hours + ":" + minutes + " " + ampm;
+//     return strTime;
+//   }
+// }
+
+// module.exports = Kaliflah;
